8f4c8cfa7656478810c43aaa94e4007fb0010703
Nunes, Ederson da Rosa <nunese01@universalleaf.com>
2020-06-01 20:53:01 -0300

    Criado métodos e atributos para Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,22 @@
+import sys
 class Pessoa():
-	pass
+	def __init__(self, nome=None, idade=44, *args, **kwargs):
+		self.idade = idade
+		self.nome = nome
+		return super().__init__(*args, **kwargs)
+
+	def cumprimentar(self):
+		return f'Olá {id(self)}'
+
+def main():
+	p = Pessoa('Marcos')
+	print(Pessoa(p))
+	print(id(p))
+	print(p.cumprimentar())
+	print(p.nome)
+	p.nome = 'Ederson'
+	print(p.nome)
+	print(p.idade)
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
